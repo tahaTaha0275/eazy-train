fb85aacf7b375c637ab23f506bb77ac6bb87d9e5
tahaTaha0275 <tahaabdelwahed0275@gmail.com>
2025-04-09 21:37:00 +0300

    refactor: css
    refactoring css files and slight changes in css

--- eazy-train-frontend/src/App.jsx
+++ eazy-train-frontend/src/App.jsx
@@ -3,6 +3,17 @@ import {Routes, Route } from 'react-router-dom'
 import Tickets from './pages/Tickets' 
 import Layout from './pages/Layout'
 import Review from "./pages/Review"
+
+import "./components/styles/BillDetails.css"
+import "./components/styles/BoardingDetails.css"
+import "./components/styles/DateSelector.css"
+import "./components/styles/Footer.css"
+import "./components/styles/Header.css"
+import "./components/styles/SearchForm.css"
+import "./components/styles/TrainList.css"
+import "./components/styles/TravellerForm.css"
+
+
 function App() {
   return (
         <div className={"container"}>
