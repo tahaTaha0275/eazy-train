e07d0f73dd69d40745d381227e4839dcc7a4ccc4
tahaTaha0275 <tahaabdelwahed0275@gmail.com>
2025-04-14 16:51:17 +0300

    Adding responsiveness

--- eazy-train-frontend/src/components/SearchForm.jsx
+++ eazy-train-frontend/src/components/SearchForm.jsx
@@ -1,8 +1,9 @@
+import "../components/styles/SearchForm.css"
 export function SearchForm() {
     return (
       <div className={"form-container"}>
         <div className={"inputGroup"}>
-          <input type="text" placeholder="From" defaultValue="Riyadh, Saudi Arabia" className={"input"} />
+          <input  type="text" placeholder="From" defaultValue="Riyadh, Saudi Arabia" className={"input"} />
           <input type="text" placeholder="To" defaultValue="Dammam, Saudi Arabia" className={"input"} />
         </div>
         <button className={"searchButton"}>Search for trains</button>
